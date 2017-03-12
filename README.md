9af0c39412e85c73ee70a9a7cc6196e2995a025d
Eran Rom <eran@itsonlyme.name>
2017-03-12 17:32:46 +0200

    Initial Revision

---
+++ README.md
@@ -0,0 +1,27 @@
+# e2emlstorlets
+Storlets and Jupyter notebook demonstrating end to end machine learning with storlets
+using IPython
+
+# Installation
+On a fresh new 16.04 VM with apasswordless sudoer simply:
+
+```
+git clone https://github.com/eranr/e2emlstorlets.git
+cd e2emlstorlets
+./install.sh
+tox -e functional
+```
+
+This will install Swift and Storlets on the VM together with
+a docker container that has the all the necessary packages
+for running the storlets in the repo.
+Those packages include: opencv, scikit-learn and dlib
+
+# COPYERIGHT Notice
+The code behind the face swap is a combination of code borrowed from
+Satya Mallic [0], [1] and from Matthew Earl [2], [3]
+
+[0] http://www.learnopencv.com/face-swap-using-opencv-c-python/
+[1] https://github.com/spmallick/learnopencv/tree/master/FaceSwap
+[2] http://matthewearl.github.io/2015/07/28/switching-eds-with-python/
+[3] https://github.com/matthewearl/faceswap/blob/master/faceswap.py
---
+++ install.sh
@@ -0,0 +1,50 @@
+#git clone https://github.com/openstack/storlets ~/storlets
+#cd ~/storlets
+#./s2aio.sh dev host
+
+NUM_IMAGES=`sudo docker images | grep  -v ubuntu | grep -v REPOSITORY | awk '{print $1}' | wc -l`
+
+if [ $NUM_IMAGES != 1 ]; then
+    echo "Cannot determine the project id. Please execute install.sh [project id]"
+    exit
+fi
+
+PROJECT_ID=`sudo docker images | grep  -v ubuntu | grep -v REPOSITORY | awk '{print $1}'`
+
+mkdir -p /tmp/update_docker_image
+cat <<EOF >/tmp/update_docker_image/Dockerfile
+FROM $PROJECT_ID
+
+MAINTAINER root
+
+# The following operations should be defined in one line
+# to prevent docker images from including apt cache file.
+RUN apt-get update && \
+    apt-get install python-numpy -y && \
+    apt-get install python-scipy -y && \
+    pip install scikit-learn==0.18 && \
+    pip in stall dlib && \
+    apt-get install --assume-yes build-essential cmake git && \
+    apt-get install --assume-yes build-essential pkg-config unzip ffmpeg qtbase5-dev python-dev && \
+    apt-get install --assume-yes libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev && \
+    apt-get install --assume-yes libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev && \
+    apt-get install --assume-yes libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev && \
+    apt-get install --assume-yes libvorbis-dev libxvidcore-dev v4l-utils && \
+    mkdir build && \
+    cd build/ && \
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_CUBLAS=ON -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES" .. && \
+    make -j $(($(nproc) + 1)) && \
+    make install && \
+    /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' && \
+    ldconfig && \
+    apt-get update && 
+
+EOF
+
+sudo docker build -t e2emlstorlets /tmp/update_docker_image
+#IMAGE_ID=`sudo docker images | grep storlet_scikit_learn | awk '{print $3}'`
+#sudo docker rmi $PROJECT_ID
+#sudo docker tag $IMAGE_ID ${PROJECT_ID:0:13}
+#sudo docker rmi e2emlstorlets
+
+#rm -fr /tmp/update_docker_image
